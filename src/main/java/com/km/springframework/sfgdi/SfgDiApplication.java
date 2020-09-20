c80400139b0319c9bab80150b3985280f477cf8a
Kmeneouali <khalledmeneouali89@gmail.com>
2020-09-20 23:55:52 +0200

    Spring Context

--- src/main/java/com/km/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/com/km/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,19 @@
 package com.km.springframework.sfgdi;
 
+import com.km.springframework.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+		ApplicationContext ctx=SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController= (MyController) ctx.getBean("myController");
+
+		System.out.println(myController.sayHello());
 	}
 
 }
---
+++ src/main/java/com/km/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,14 @@
+package com.km.springframework.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+
+    public String sayHello(){
+        return "Hi Khalled !!! ";
+    }
+
+
+}
