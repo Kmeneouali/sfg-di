9e00ad61008931ba2568d22c68b24332f6d9d00e
Kmeneouali <khalledmeneouali89@gmail.com>
2020-09-21 00:20:41 +0200

    Spring DI without Spring : by Setter injected

---
+++ src/main/java/com/km/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -0,0 +1,16 @@
+package com.km.springframework.sfgdi.controllers;
+
+import com.km.springframework.sfgdi.services.GreetingService;
+
+public class SetterInjectedController {
+
+    public GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
