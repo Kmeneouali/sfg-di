3ced489ea9d6a37d94adcdbf9c2f09173a6d07e9
Kmeneouali <khalledmeneouali89@gmail.com>
2020-09-21 20:02:03 +0200

    @Profile

--- src/main/java/com/km/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/com/km/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package com.km.springframework.sfgdi;
 
-import com.km.springframework.sfgdi.controllers.ConstructorInjectedController;
-import com.km.springframework.sfgdi.controllers.MyController;
-import com.km.springframework.sfgdi.controllers.PropertyInjectedController;
-import com.km.springframework.sfgdi.controllers.SetterInjectedController;
+import com.km.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,10 +11,12 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext ctx=SpringApplication.run(SfgDiApplication.class, args);
-		MyController myController= (MyController) ctx.getBean("myController");
+
+		I18nController i18nController= (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
 		System.out.println("------- Primary ");
-
+		MyController myController= (MyController) ctx.getBean("myController");
 		System.out.println(myController.sayHello());
 
 
